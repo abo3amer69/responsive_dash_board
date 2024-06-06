8003c0efdf0aeafb224a54c038068fd9d8a19a8d
Ahmed <01099517574med@gmail.com>
2024-06-06 14:41:15 +0300

    add assets style and assets images and fonts

--- lib/views/dashboard_view.dart
+++ lib/views/dashboard_view.dart
@@ -1,10 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:responsive_dash_board/widgets/adaptive_layout_widget.dart';
+import 'package:responsive_dash_board/widgets/dashboard_desktop_layout.dart';
 
 class DashBoardView extends StatelessWidget {
   const DashBoardView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      body: AdaptiveLayout(
+          mobiellayout: (context) => const SizedBox(),
+          tabletlayout: (context) => const SizedBox(),
+          desktoplayout: (context) => const DashBoardDesktopLayout()),
+    );
   }
 }
