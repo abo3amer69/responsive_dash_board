84ba83e91dda222fe08916987161394756eea6a2
Ahmed <01099517574med@gmail.com>
2024-06-12 14:50:25 +0300

    create active drawer item

---
+++ lib/widgets/active_and_anactive_item.dart
@@ -0,0 +1,50 @@
+
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+import 'package:responsive_dash_board/model/drawer_item_model.dart';
+import 'package:responsive_dash_board/utils/app-style.dart';
+
+class AnActiveDrawerItem extends StatelessWidget {
+  const AnActiveDrawerItem({
+    super.key,
+    required this.draweritemModel,
+  });
+
+  final DrawerItemModel draweritemModel;
+
+  @override
+  Widget build(BuildContext context) {
+    return ListTile(
+      leading: SvgPicture.asset(draweritemModel.image),
+      title: Text(
+        draweritemModel.title,
+        style: AppStyles.styleMedium16,
+      ),
+    );
+  }
+}
+
+
+class ActiveDrawerItem extends StatelessWidget {
+  const ActiveDrawerItem({
+    super.key,
+    required this.draweritemModel,
+  });
+
+  final DrawerItemModel draweritemModel;
+
+  @override
+  Widget build(BuildContext context) {
+    return ListTile(
+      leading: SvgPicture.asset(draweritemModel.image),
+      title: Text(
+        draweritemModel.title,
+        style: AppStyles.styleBold16,
+      ),
+      trailing: Container(
+        width: 3.27,
+        decoration: const BoxDecoration(color: Color(0xff4EB7F2)),
+      ),
+    );
+  }
+}
--- lib/widgets/drawer_item.dart
+++ lib/widgets/drawer_item.dart
@@ -2,21 +2,20 @@ import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:responsive_dash_board/model/drawer_item_model.dart';
 import 'package:responsive_dash_board/utils/app-style.dart';
+import 'package:responsive_dash_board/widgets/active_and_anactive_item.dart';
 
 class DrawerItem extends StatelessWidget {
-  const DrawerItem({super.key, required this.draweritemModel, required this.isActive});
+  const DrawerItem(
+      {super.key, required this.draweritemModel, required this.isActive});
 
   final DrawerItemModel draweritemModel;
   final bool isActive;
 
   @override
   Widget build(BuildContext context) {
-    return ListTile(
-      leading: SvgPicture.asset(draweritemModel.image),
-      title: Text(
-        draweritemModel.title,
-        style: AppStyles.styleMedium16,
-      ),
-    );
+    return isActive
+        ? ActiveDrawerItem(draweritemModel: draweritemModel)
+        : AnActiveDrawerItem(draweritemModel: draweritemModel);
   }
 }
+
