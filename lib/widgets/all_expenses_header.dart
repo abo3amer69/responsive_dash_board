02634a96b0b0139c24a4ddfb04dcb660efada49f
Ahmed <01099517574med@gmail.com>
2024-06-12 22:15:23 +0300

    create expanses part 2 Range Options

--- lib/widgets/all_expenses_header.dart
+++ lib/widgets/all_expenses_header.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:responsive_dash_board/utils/app-style.dart';
+import 'package:responsive_dash_board/widgets/range_option.dart';
 
 class AllExpensesHeader extends StatelessWidget {
   const AllExpensesHeader({super.key});
@@ -6,7 +8,14 @@ class AllExpensesHeader extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const Row(
-      children: [],
+      children: [
+         Text(
+          'All Expenses',
+          style: AppStyles.styleSemiBold20,
+        ),
+         Expanded(child: SizedBox()),
+        RangeOptions()
+      ],
     );
   }
 }
