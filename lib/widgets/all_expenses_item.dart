2eab5065ed4575a9ec3a7938154cc8cce9fd3f9a
Ahmed <01099517574med@gmail.com>
2024-06-13 02:11:44 +0300

    create item header and model item header

---
+++ lib/widgets/all_expenses_item.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+import 'package:responsive_dash_board/model/all_expenses_item_model.dart';
+import 'package:responsive_dash_board/widgets/all_expenses.dart';
+import 'package:responsive_dash_board/widgets/all_expenses_item_header.dart';
+
+class AllExpensesItem extends StatelessWidget {
+  const AllExpensesItem({super.key, required this.itemModel});
+
+  final AllExpensesItemModel itemModel;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 16),
+      decoration: ShapeDecoration(
+        color: Colors.white,
+        shape: RoundedRectangleBorder(
+          side: const BorderSide(
+            width: 1,
+            color: Color(0xffF1F1F1),
+          ),
+          borderRadius: BorderRadius.circular(12),
+        ),
+      ),
+      child: Column(
+        children: [
+          AllExpensesItemHeader(
+            image: itemModel.image,
+          ),
+        ],
+      ),
+    );
+  }
+}
