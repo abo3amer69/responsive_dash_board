ad92f4e6896c0ccd12591810d25675d08493222f
Ahmed <01099517574med@gmail.com>
2024-06-17 14:10:13 +0300

    create model user info model

--- lib/widgets/custom_drawer.dart
+++ lib/widgets/custom_drawer.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:responsive_dash_board/model/drawer_item_model.dart';
+import 'package:responsive_dash_board/model/user_info_model.dart';
 import 'package:responsive_dash_board/utils/app_images.dart';
 import 'package:responsive_dash_board/widgets/active_and_anactive_item.dart';
 import 'package:responsive_dash_board/widgets/drawer_item.dart';
@@ -17,9 +18,11 @@ class CustomDrawer extends StatelessWidget {
         slivers: [
           SliverToBoxAdapter(
             child: UserInfoListTile(
-                image: Assets.imagesAvatar1,
-                title: 'Lekan Okeowo',
-                subtitle: 'demo@gmail.com'),
+              userInfoModel: UserInfoModel(
+                  image: Assets.imagesAvatar1,
+                  title: 'Lekan Okeowo',
+                  subtitle: 'demo@gmail.com'),
+            ),
           ),
           SliverToBoxAdapter(
             child: SizedBox(
@@ -27,21 +30,25 @@ class CustomDrawer extends StatelessWidget {
             ),
           ),
           DrawerItemListView(),
-
-          
           SliverFillRemaining(
             hasScrollBody: false,
             child: Column(
               children: [
-                Expanded(child: SizedBox(height: 20,)),
+                Expanded(
+                    child: SizedBox(
+                  height: 20,
+                )),
                 AnActiveDrawerItem(
-                    draweritemModel: DrawerItemModel(
-                      image: Assets.imagesSettings,title: 'Setting system'),
-                  ),
-                  AnActiveDrawerItem(
-                    draweritemModel: DrawerItemModel(image: Assets.imagesLogout,title: 'LogOut account'),
-                  ),
-                  SizedBox(height: 48,),
+                  draweritemModel: DrawerItemModel(
+                      image: Assets.imagesSettings, title: 'Setting system'),
+                ),
+                AnActiveDrawerItem(
+                  draweritemModel: DrawerItemModel(
+                      image: Assets.imagesLogout, title: 'LogOut account'),
+                ),
+                SizedBox(
+                  height: 48,
+                ),
               ],
             ),
           )
--- lib/widgets/user_info_list_tile.dart
+++ lib/widgets/user_info_list_tile.dart
@@ -1,15 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
+import 'package:responsive_dash_board/model/user_info_model.dart';
 import 'package:responsive_dash_board/utils/app-style.dart';
 
 class UserInfoListTile extends StatelessWidget {
   const UserInfoListTile(
-      {super.key,
-      required this.image,
-      required this.title,
-      required this.subtitle});
+      {super.key, required this.userInfoModel,});
 
-  final String image, title, subtitle;
+  final UserInfoModel userInfoModel;
 
   @override
   Widget build(BuildContext context) {
@@ -17,13 +15,13 @@ class UserInfoListTile extends StatelessWidget {
       color: const Color(0xffFAFAFA),
       elevation: 0,
       child: ListTile(
-        leading: SvgPicture.asset(image),
+        leading: SvgPicture.asset(userInfoModel.image),
         title: Text(
-          title,
+          userInfoModel.title,
           style: AppStyles.styleSemiBold16,
         ),
         subtitle: Text(
-          subtitle,
+          userInfoModel.subtitle,
           style: AppStyles.styleRegular12,
         ),
       ),
