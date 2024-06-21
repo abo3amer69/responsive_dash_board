95007e6e8f72fc9a630345b454b9527de43c2925
Ahmed <01099517574med@gmail.com>
2024-06-22 00:51:17 +0300

    create transaction history list view

--- lib/widgets/transaction_history.dart
+++ lib/widgets/transaction_history.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:responsive_dash_board/utils/app-style.dart';
 import 'package:responsive_dash_board/widgets/transaction_history_header.dart';
+import 'package:responsive_dash_board/widgets/transaction_history_list_view.dart';
 
 class TransactionHistory extends StatelessWidget {
   const TransactionHistory({super.key});
@@ -15,6 +16,7 @@ class TransactionHistory extends StatelessWidget {
           '13 April 2022',
           style: AppStyles.styleRegular16,
         ),
+        TransactionHistoryListView(),
       ],
     );
   }
---
+++ lib/widgets/transaction_history_list_view.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+import 'package:responsive_dash_board/model/transaction_model.dart';
+import 'package:responsive_dash_board/widgets/transaction_item.dart';
+
+class TransactionHistoryListView extends StatelessWidget {
+  const TransactionHistoryListView({super.key});
+
+  static const items = [
+    TransactionModel(
+        title: 'Cash Withdrawal',
+        date: '13 Apr, 2022 ',
+        amount: r'$20,129',
+        isWhithdrawal: true),
+    TransactionModel(
+        title: 'Cash Withdrawal',
+        date: '13 Apr, 2022 ',
+        amount: r'$20,129',
+        isWhithdrawal: true),
+    TransactionModel(
+        title: 'Cash Withdrawal',
+        date: '13 Apr, 2022 ',
+        amount: r'$20,129',
+        isWhithdrawal: false)
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      itemCount: items.length,
+      shrinkWrap: true,
+      itemBuilder: ((context, index) {
+        return TransactionItem(transactionModel: items[index]);
+      }),
+    );
+  }
+}
