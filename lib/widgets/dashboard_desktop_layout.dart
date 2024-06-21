a096b41e136df0eded4196eebbc5f79ec5bdb5f7
Ahmed <01099517574med@gmail.com>
2024-06-21 15:25:10 +0300

    create three dots indicator

--- lib/widgets/dashboard_desktop_layout.dart
+++ lib/widgets/dashboard_desktop_layout.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:responsive_dash_board/widgets/all_expenses.dart';
 import 'package:responsive_dash_board/widgets/all_expensess_and_quick_invoice.dart';
 import 'package:responsive_dash_board/widgets/custom_drawer.dart';
+import 'package:responsive_dash_board/widgets/dots_indicator.dart';
 import 'package:responsive_dash_board/widgets/my_card.dart';
 import 'package:responsive_dash_board/widgets/my_cards_page_view.dart';
 import 'package:responsive_dash_board/widgets/quick_invoice.dart';
@@ -17,11 +18,11 @@ class DashBoardDesktopLayout extends StatelessWidget {
         SizedBox(
           width: 32,
         ),
-        Expanded(
-            flex: 2,
-            child: AllExpensesAndQuickInvoiceSection()),
-            SizedBox(width: 24,),
-            Expanded(child: MyCardsPageView()),
+        Expanded(flex: 2, child: AllExpensesAndQuickInvoiceSection()),
+        SizedBox(
+          width: 24,
+        ),
+        DotsIndicator(),
       ],
     );
   }
