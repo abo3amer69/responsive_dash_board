35b8ad4fc4eed311f000811fc76428452cd764a6
Ahmed <01099517574med@gmail.com>
2024-06-22 02:23:40 +0300

    create income section header

--- lib/widgets/dashboard_desktop_layout.dart
+++ lib/widgets/dashboard_desktop_layout.dart
@@ -3,6 +3,7 @@ import 'package:responsive_dash_board/widgets/all_expenses.dart';
 import 'package:responsive_dash_board/widgets/all_expensess_and_quick_invoice.dart';
 import 'package:responsive_dash_board/widgets/custom_drawer.dart';
 import 'package:responsive_dash_board/widgets/dots_indicator.dart';
+import 'package:responsive_dash_board/widgets/income_section.dart';
 import 'package:responsive_dash_board/widgets/my_card.dart';
 import 'package:responsive_dash_board/widgets/my_cards_page_view.dart';
 import 'package:responsive_dash_board/widgets/my_cards_section.dart';
@@ -28,7 +29,7 @@ class DashBoardDesktopLayout extends StatelessWidget {
         SizedBox(
           width: 24,
         ),
-        Expanded(child: MyCardsTransactionHistorySection()),
+        Expanded(child: IncomeSection()),
       ],
     );
   }
---
+++ lib/widgets/income_section_header.dart
@@ -0,0 +1,52 @@
+
+import 'package:flutter/material.dart';
+import 'package:responsive_dash_board/utils/app-style.dart';
+
+class IncomeSectionHeader extends StatelessWidget {
+  const IncomeSectionHeader({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: [
+        const Text(
+          'Income',
+          style: AppStyles.styleSemiBold20,
+        ),
+        Container(
+          padding: const EdgeInsets.symmetric(
+            horizontal: 16,
+            vertical: 12,
+          ),
+          decoration: ShapeDecoration(
+            color: Colors.white,
+            shape: RoundedRectangleBorder(
+              side: const BorderSide(
+                width: 1,
+                color: Color(0xffF1F1F1),
+              ),
+              borderRadius: BorderRadius.circular(12),
+            ),
+          ),
+          child: Row(
+            children: [
+              const Text(
+                'Monthly',
+                style: AppStyles.styleMedium16,
+              ),
+              const SizedBox(
+                height: 16,
+              ),
+              Transform.rotate(
+                  angle: -1.57079633,
+                  child: const Icon(Icons.arrow_back_ios_new))
+            ],
+          ),
+        )
+      ],
+    );
+  }
+}
